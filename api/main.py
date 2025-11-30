71abcc43e6a0083a405b20f2c08f17428baf26a2
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-11-30 21:37:06 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
-from scraper.sofascore import get_team_id_by_name
-from scraper.matches import get_form_sequence
+from scraper.api_football import get_last_five_matches
 
 app = FastAPI()
+
+
+@app.get("/form/{team}")
+def form(team: str):
+    data = get_last_five_matches(team)
+
+    if not data:
+        return {"error": "Takım bulunamadı"}
+
+    return {
+        "team": data["team_name"],
+        "form": data["form_string"],
+        "matches": data["matches"],
+        "logo": data["logo"],
+    }
+
 
 @app.get("/form/{team}")
 def form(team: str):
@@ -17,3 +32,4 @@ def form(team: str):
         "team": team,
         "form": form_data
     }
+
