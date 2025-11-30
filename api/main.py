1e060420f9d7a0c3a3820f8ac06706969c4a66b1
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-12-01 00:18:49 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
-from scraper.api_football import get_last_five_matches
+from scraper.api_football import (
+    get_last_five_matches,
+    get_super_lig_season,
+    debug_team_search
+)
 
 app = FastAPI()
 
@@ -10,14 +14,12 @@ def root():
 @app.get("/form/{team}")
 def form(team: str):
     data = get_last_five_matches(team)
-
-    if "error" in data:
-        return {"error": "Takım bulunamadı"}
-
     return data
-
-from scraper.api_football import get_super_lig_season
 
 @app.get("/ligtest")
 def ligtest():
     return get_super_lig_season()
+
+@app.get("/debug/{team}")
+def debug(team: str):
+    return debug_team_search(team)
