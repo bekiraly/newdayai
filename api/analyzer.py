7e94e25a9ea9906f1e8f00d3c04a49d7e7a98713
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-12-03 21:20:28 +0300

    Update analyzer.py

--- api/analyzer.py
+++ api/analyzer.py
@@ -3,7 +3,6 @@ from scraper.sites.sofascore import SofaScoreScraper
 from scraper.sites.nesine import NesineScraper
 from scraper.browser import Browser
 
-
 async def analyze_match(home: str, away: str) -> RawAggregateData:
     """
     Tüm sitelerden veri toplayıp tekleştiriyoruz.
