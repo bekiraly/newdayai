4b16e93abd95f264a644838944642d3e887ad345
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-12-03 19:49:02 +0300

    Update analyzer.py

--- api/analyzer.py
+++ api/analyzer.py
@@ -9,9 +9,10 @@ async def analyze_match(home: str, away: str) -> RawAggregateData:
     Tüm sitelerden veri toplayıp tekleştiriyoruz.
     """
 
-    async with Browser() as browser:
-        ss = SofaScoreScraper(browser)
-        ns = NesineScraper(browser)
+    async with Browser() as page:
+       ss = SofaScoreScraper(page)
+       ns = NesineScraper(page)
+
 
         # Sofascore form
         form_home = await ss.get_team_form(home)
