97f76be8733e133251420efe663de837196c1009
Bekir yenigün <148310893+bekiraly@users.noreply.github.com>
2025-12-03 20:42:38 +0300

    Update browser.py

--- scraper/browser.py
+++ scraper/browser.py
@@ -2,17 +2,15 @@ from playwright.async_api import async_playwright
 
 class Browser:
     async def __aenter__(self):
-        self.play = await async_playwright().start()
-        self.browser = await self.play.chromium.launch(
+        self.playwright = await async_playwright().start()
+        self.browser = await self.playwright.chromium.launch(
             headless=True,
-            args=[
-                "--no-sandbox",
-                "--disable-dev-shm-usage"
-            ]
+            executable_path="/tmp/pw/chromium/headless_shell",
+            args=["--no-sandbox", "--disable-dev-shm-usage"]
         )
         self.page = await self.browser.new_page()
         return self.page
 
     async def __aexit__(self, exc_type, exc, tb):
         await self.browser.close()
-        await self.play.stop()
+        await self.playwright.stop()
